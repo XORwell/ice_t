bed9380cce4bb8f5f81f06f79bd57d5dceffb0c0
Christian Nennemann <christian.nennemann@gmail.com>
2013-12-07 22:15:43 +0100

    removed obsolete method from TimeHelper

--- lib/ice_t/time_helper.rb
+++ lib/ice_t/time_helper.rb
@@ -10,37 +10,5 @@ module IceT
     def get_months_between(start_time, end_time)
       (end_time.year * 12 + end_time.month) - (start_time.year * 12 + start_time.month)
     end
-
-
-    def human_understanding_of_time_equality(a, b)
-      int_a = a.interval
-      int_b = b.interval
-      case
-
-      when a.is_a?(IceT::Rules::YearlyRule) && b.is_a?(IceT::Rules::MonthlyRule)
-           return (int_a.to_f / int_b.to_f) == 0.08333333333333333
-      when a.is_a?(IceT::Rules::YearlyRule) && b.is_a?(IceT::Rule::Daily)
-           return (int_a.to_f / int_b.to_f) == 0.03287671232876712
-           
-      # when a.is_a?(IceT::Rules::YearlyRule) && b.is_a?(IceT::Rule::Minutely)
-      #      return (int_a.to_f / int_b.to_f) == 0.03287671232876712
-
-
-      when a.is_a?(IceT::Rules::MonthlyRule) && b.is_a?(IceT::Rules::YearlyRule)
-           return (int_a / int_b) == 12
-
-      when a.is_a?(IceT::Rule::Daily) && b.is_a?(IceT::Rules::YearlyRule)
-           return (int_a / int_b) == 365
-      
-      else
-        return false
-      end
-
-    end
-    def human_understanding_of_date_equality
-      return "fireball"
-    end
-    
-
   end
 end
